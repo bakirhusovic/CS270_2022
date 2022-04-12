c2f11d2674c3212bc8326bdeb17c1effa6d940a1
bakir.husovic <bakir.husovic@ssst.edu.ba>
2022-04-12 13:25:57 +0200

    php & forms

---
+++ week7/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $test = 'Hello';
+        $test2 = 'world';
+      //echo $test . ' ' . $test2;
+        echo "{$test}2 $test2";
+        $arrayTest = [10, 15, 20, 'test'];
+
+        var_dump($arrayTest);
+
+        $a = 0;
+
+
+    ?>
+</body>
+</html>
